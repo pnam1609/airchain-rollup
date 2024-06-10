c1d1d192b7d5457b292bf38fb16519bc20d1c4f3
Hoang Phuong Nam <pnam16091999@gmail.com>
2024-06-10 22:08:16 +0700

    edit

--- command/config/config.sh
+++ command/config/config.sh
@@ -9,10 +9,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confd
 
 git clone https://github.com/airchains-network/evm-station.git
 git clone https://github.com/airchains-network/tracks.git
-echo "export MONIKER=cashze7" >> /root/.bashrc
-echo "export RECEIVER_ADDRESS=0x0bd70A367E62DbF683677AE85B6436faD5a267C9" >> /root/.bashrc
-echo "export USERIP=95.217.191.138" >> /root/.bashrc
-echo "export RPC_AIR=https://gpc.junkio.tech/" >> /root/.bashrc
 echo 'export RPC_EVM=http://$USERIP:8545' >> /root/.bashrc
 echo "export DA_TYPE=celestia" >> /root/.bashrc
 echo "export RPC_DA=https://celestia-testnet-rpc.itrocket.net" >> /root/.bashrc
